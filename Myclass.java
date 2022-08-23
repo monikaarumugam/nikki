9d6f6743bf2ddc012e18c0b055736b5cff2de011
monikaarumugam <111746215+monikaarumugam@users.noreply.github.com>
2022-08-22 23:11:17 -1000

    Add files via upload

---
+++ Myclass.java
@@ -0,0 +1,16 @@
+package myfirstpackage;
+
+public class Myclass {
+	 public void myMethod() {
+		  System.out.println("This is myMethod ");
+		 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is main method ");
+		Myclass m = new Myclass();
+		m.myMethod();
+
+	}
+
+}
