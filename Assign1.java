dee5ef10dc7e66d1aa6976e5c6da3c56b17b3dfb
monikaarumugam <111746215+monikaarumugam@users.noreply.github.com>
2022-08-24 00:52:22 -1000

    Add files via upload

---
+++ Assign1.java
@@ -0,0 +1,17 @@
+package assignment1;
+
+public class Assign1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char e = '\u0053';
+		char f = '\u0048';
+		char g = '\u0056';
+		System.out.println("Captial Letter of my name using CHAR value:" +e+f+g);
+		char m = '\u0068';
+		char n = '\u0076';
+		
+		System.out.println("Small  Letter of my name using CHAR value:" +m+n);
+	}
+
+}
